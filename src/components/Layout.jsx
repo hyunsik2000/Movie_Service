4d3e5e42f3d8d2c95b18aec10e66386827675d8c
hyunsik2000 <ryuhsik2000@naver.com>
2025-12-03 16:30:32 +0900

    fix: Layout 수정하여 NavBar를 적용

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router";
+import { NavBar } from "./NavBar";
 
 function Layout() {
   return (
-    <>
-      <main className="flex-center min-h-screen min-w-[320px] bg-[#141517] text-white">
+    <div className="min-h-screen min-w-[320px] bg-[#141517] text-white">
+      <NavBar />
+      <main className="flex-center mt-[70px]">
         <Outlet />
       </main>
-    </>
+    </div>
   );
 }
 export default Layout;
