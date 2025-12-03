ae082f6556dacc8b331265d59231c938b70f69ee
hyunsik2000 <ryuhsik2000@naver.com>
2025-12-03 14:00:08 +0900

    feat: App.jsx 레이아웃 구성 및 1단계 요구사항 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import "./App.css";
+import { MovieCard } from "./components/MovieCard";
+import movieList from "./data/movieListData.json";
+
 function App() {
-  return <>FE15/류현식</>;
+  return (
+    <div className="flex-center min-h-screen bg-[#141517] text-white">
+      <div className="flex-between max-w-[1200px] flex-wrap gap-8">
+        {movieList.results.map((movieItem) => (
+          <MovieCard key={movieItem.id} movieItem={movieItem} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,21 @@
+import { TMDB_IMAGE_BASE_URL } from "../constants/tmdb";
+
+export function MovieCard({ movieItem }) {
+  const imgSrc = `${TMDB_IMAGE_BASE_URL}${movieItem.poster_path}`;
+
+  return (
+    <section className="w-[250px]">
+      <ul>
+        <li>
+          <img
+            src={imgSrc}
+            alt={movieItem}
+            className="h-[375px] object-cover"
+          />
+        </li>
+        <li className="">{movieItem.original_title}</li>
+        <li>{movieItem.vote_average}</li>
+      </ul>
+    </section>
+  );
+}
