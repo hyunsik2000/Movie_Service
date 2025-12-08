686cd380e70d8cea41af9ccedbb9afef25cc3e5f
hyunsik2000 <ryuhsik2000@naver.com>
2025-12-08 18:37:36 +0900

    feat: search 경로 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./pages/Home";
 import Layout from "./components/Layout";
 import Detail from "./pages/Detail";
+import { Search } from "./pages/Search";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="details/:id" element={<Detail />} />
+        <Route path="search" element={<Search />} />
       </Route>
     </Routes>
   );
