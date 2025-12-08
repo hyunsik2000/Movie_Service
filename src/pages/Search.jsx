cc7a24327948ec3888ca059244034cbec6b87061
hyunsik2000 <ryuhsik2000@naver.com>
2025-12-09 04:56:09 +0900

    fix: pages 절대 경로 수정

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,8 +1,8 @@
 import { useSearchParams } from "react-router-dom";
-import { useDebounce } from "../hooks/useDebounce";
-import { useSearchFetch } from "../hooks/useSearchFetch";
-import { SearchLoading } from "../components/MovieSearch/SearchLoading";
-import { SearchMovieList } from "../components/MovieSearch/SearchMovieList";
+import { useDebounce } from "@hooks/useDebounce";
+import { useSearchFetch } from "@hooks/useSearchFetch";
+import { SearchLoading } from "@components/MovieSearch/SearchLoading";
+import { SearchMovieList } from "@components/MovieSearch/SearchMovieList";
 
 export function Search() {
   const [searchParams] = useSearchParams();
@@ -13,7 +13,7 @@ export function Search() {
   const { data: results, loading, error } = useSearchFetch(debouncedKeyword);
 
   return (
-    <section className="mx-auto max-w-5xl px-6 py-10 text-white">
+    <section className="mx-auto max-w-5xl px-6 py-10">
       {loading && <SearchLoading />}
       {error && <p>오류가 발생했습니다. {error.message}</p>}
       {!loading && <SearchMovieList results={results} />}
