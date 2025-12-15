3d79842f2124d6317d9957c9149e559ac327309e
hyunsik2000 <ryuhsik2000@naver.com>
2025-12-15 16:35:49 +0900

    fix: movieDetail창 스켈레톤 css 방법 변경 - mock 데이터로 크기 맞추기

--- src/components/MovieDetail/MovieDetailSkeleton.jsx
+++ src/components/MovieDetail/MovieDetailSkeleton.jsx
@@ -1,30 +1,46 @@
 export function MovieDetailSkeleton() {
+  const mockMovieDetail = {
+    title: "가상의 영화 제목",
+    vote_average: 8.8,
+    genres: [
+      { id: 1, name: "액션" },
+      { id: 2, name: "드라마" },
+      { id: 3, name: "스릴러" },
+    ],
+  };
+
   return (
-    <section className="max-auto flex w-5xl animate-pulse flex-col items-center sm:flex-row">
-      <div className="h-[768px] w-full p-[10px_20px] sm:w-[50%]">
-        <div className="h-full w-full bg-gray-400 dark:bg-[#272A30]" />
+    <section className="mx-auto flex w-5xl animate-pulse flex-col items-center gap-12 p-[10px_20px] sm:flex-row sm:items-start">
+      <div className="aspect-2/3 w-full sm:w-[50%]">
+        <div className="skeleton-text-bg h-full w-full" />
       </div>
 
-      <ul className="flex w-full flex-col gap-4 p-[10px_20px] sm:w-[50%]">
+      <ul className="flex w-full flex-1 flex-col gap-4 sm:mt-10 sm:w-[50%]">
         <li className="flex-between">
-          <p className="flex-between h-10 w-4/5 rounded bg-gray-500 text-xl md:text-3xl lg:text-4xl dark:bg-[#1E2024]" />
-          <p className="flex-between h-8 w-1/10 rounded bg-gray-500 text-xl md:text-xl lg:text-2xl dark:bg-[#1E2024]" />
+          <p className="skeleton-text-bg text-xl font-semibold md:text-3xl lg:text-4xl">
+            {mockMovieDetail.title}
+          </p>
+          <p className="skeleton-text-bg text-lg font-semibold md:text-xl lg:text-2xl">
+            {mockMovieDetail.vote_average}
+          </p>
         </li>
 
-        <li className="flex flex-wrap items-center gap-4 gap-y-2">
-          <span className="h-7 w-1/6 rounded bg-gray-500 dark:bg-[#1E2024]" />
-          <span className="h-7 w-1/6 rounded bg-gray-500 dark:bg-[#1E2024]" />
-          <span className="h-7 w-1/6 rounded bg-gray-500 dark:bg-[#1E2024]" />
-          <span className="h-7 w-1/6 rounded bg-gray-500 dark:bg-[#1E2024]" />
-          <span className="h-7 w-1/6 rounded bg-gray-500 dark:bg-[#1E2024]" />
+        <li className="flex flex-wrap items-center gap-3 gap-y-2">
+          {mockMovieDetail.genres.map((el) => (
+            <span
+              key={el.id}
+              className="skeleton-text-bg rounded-full px-3 py-1 text-xs font-medium md:text-sm"
+            >
+              {el.name}
+            </span>
+          ))}
         </li>
 
         <li>
           <div className="flex flex-col gap-2">
-            <p className="h-8 w-full rounded bg-gray-400 dark:bg-[#272A30]" />
-            <p className="h-8 w-full rounded bg-gray-400 dark:bg-[#272A30]" />
-            <p className="h-8 w-[80%] rounded bg-gray-400 dark:bg-[#272A30]" />
-            <p className="h-8 w-[50%] rounded bg-gray-400 dark:bg-[#272A30]" />
+            <p className="h-6 w-full rounded bg-gray-400 dark:bg-[#272A30]" />
+            <p className="h-6 w-full rounded bg-gray-400 dark:bg-[#272A30]" />
+            <p className="h-6 w-[80%] rounded bg-gray-400 dark:bg-[#272A30]" />
           </div>
         </li>
       </ul>
